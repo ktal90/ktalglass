79c514581ca07089309383a2da020c3654d52fdb
Karan Talati <karan.talati@gmail.com>
2013-09-05 02:30:03 -0400

    first c9 commit

--- ktalglass.py
+++ ktalglass.py
@@ -1,3 +1,17 @@
+#! /usr/bin/env python
 #timeline_item = {'text': 'Hello world'}
 #service.timeline().insert(body=timeline_item).execute()
+import webapp2
 print "Hello, World!"
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
